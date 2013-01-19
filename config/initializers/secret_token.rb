8e8b22800fa78539b0cb8588da935c4726d407a8
Courtney Jones <courtney@wearefound.com>
2013-01-19 18:22:08 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cojo::Application.config.secret_token = '002257a1722c768bc4ff5c4a59074259335504cae1e688de60f0366187ec3a82f17fef5f4701eb234379886d151cbc39355c0c7ccab3b5dd28731c98cdf32988'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Cojo::Application.routes.draw do
+  resources :pages
+  
+  root to: "pages#index"
+end
